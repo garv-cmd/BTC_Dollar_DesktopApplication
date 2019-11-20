c9dd17b569914437fc4b30505473fd4359ae3119
garv-cmd <54171077+garv-cmd@users.noreply.github.com>
2019-11-20 21:56:51 +0530

    BTC to Dollars desktop application

---
+++ BTCtoDOLLAR/src/index.js
@@ -0,0 +1,45 @@
+
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.remote.BrowserWindow
+const axios = require('axios')
+const ipc = electron.ipcRenderer
+const notifyBtn = document.getElementById('notifyBtn')
+var price = document.querySelector('h1')
+var targetPrice = document.getElementById('targetPrice')
+var targetPriceVal;
+
+const notification = {
+    title: 'BTC Alert',
+    body: 'BTC just beat your target price!',
+    icon:path.join(__dirname,'../images/btc.png')
+}
+
+function getBTC() {
+  axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+  .then(res => {
+      const cryptos = res.data.BTC.USD
+      price.innerHTML = '$'+cryptos.toLocaleString('en')
+
+      // Add this:
+      if (targetPrice.innerHTML != '' && targetPriceVal < res.data.BTC.USD) {
+          const myNotification = new window.Notification(notification.title, notification)
+      }
+
+  })
+}
+getBTC()
+setInterval(getBTC,3000); 
+    notifyBtn.addEventListener('click', function (event) {
+        const modalPath = path.join('file://', __dirname, 'add.html')
+        let win = new BrowserWindow({ frame:false,alwaysOnTop:true,width: 400, height: 200,webPreferences: {
+          nodeIntegration: true
+          } })
+        win.on('close', function () { win = null })
+        win.loadURL(modalPath)
+        win.show()
+      })
+ipc.on('targetPriceVal',function(event,arg){
+  targetPriceVal = Number(arg)
+  targetPrice.innerHTML = '$'+targetPriceVal.toLocaleString('en')
+})
